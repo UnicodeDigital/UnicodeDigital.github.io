986f9874d603f07f7ad6c1250698d922d607eadd
Billy1900 <billyrobertluo@gmail.com>
2024-09-10 19:55:40 +0800

    change all image path

--- src/navigation.ts
+++ src/navigation.ts
@@ -177,7 +177,6 @@ export const footerData = {
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/UnicodeDigital' },
     { ariaLabel: 'linkedin', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/company/unicodedigital/' },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/UnicodeDigital' },
-    
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="./images/icon.png" alt="onWidget logo" loading="lazy"></img>
