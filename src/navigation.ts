4a0ae9aefcf85a5a8ecd69686e9741e271322827
Billy1900 <billyrobertluo@gmail.com>
2024-09-10 19:06:37 +0800

    change image path

--- src/navigation.ts
+++ src/navigation.ts
@@ -59,7 +59,7 @@ export const headerData = {
       // ],
     //},
     {
-      text: 'Business',
+      text: 'Blogs',
       href: getBlogPermalink(),
       // links: [
       //   {
@@ -180,7 +180,7 @@ export const footerData = {
     
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="src/assets/images/icon.png" alt="onWidget logo" loading="lazy"></img>
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="./src/assets/images/icon.png" alt="onWidget logo" loading="lazy"></img>
     Made by <a class="text-blue-600 underline dark:text-muted"> Unicode Digital</a> · All rights reserved.
   `,
 };
